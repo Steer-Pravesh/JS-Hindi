677b79a56192c3f65e5a914d4dff73774ad89c0c
Pravesh <praveshbansh@gmail.com>
2026-06-07 05:51:02 +0000

    var done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+console.log("Pravesh");//used for print statement
+var id = "1234";//var not used for its limited scope
+let id2="2333"// let is used to store variables
+const name = "Pravesh";//constant value
+accId="234"//we can write also without declaring datatype
+console.log(id);
+
+let namePerson//its value will be shown undefined a
+console.table([accId,id2,id,namePerson])
