13642c2929cfeb44829b9442667025980e13f6e5
Pravesh <praveshbansh@gmail.com>
2026-06-09 13:14:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes and EMCA.js
@@ -0,0 +1,9 @@
+"use strict"; //uses newer version of js
+//alert(3+3)// not working because we're using node not browser
+//there are different datatypes
+// number : 2 to 2^53
+// bigint // null (type= undefined),it is standalone value
+// undefined(type= undefined) //symbol => unique
+//object
+console.log(typeof null)
+console.log(typeof undefined )
