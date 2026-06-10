8cc7b1a1956b5b27d87a4b5d5fe3d633b417c8af
Pravesh <praveshbansh@gmail.com>
2026-06-10 13:45:39 +0000

    nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,8 @@
+num1=100
+num2 = new Number(100)//typeof = number
+console.log(num1)
+console.log(num2)
+console.log(num1.toString().length)
+ let num10 = 100000
+ console.log(num10.toPrecision(1))// rounds off
+ console.log(num10.toLocaleString('en-IN'))//shows number inindian standard with comma
