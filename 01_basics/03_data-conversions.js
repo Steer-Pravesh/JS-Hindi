36863b6cbd9ac7ee7a0cb5463cd5c824c5cbf53b
Pravesh <praveshbansh@gmail.com>
2026-06-10 07:16:27 +0000

    datatypes

--- 01_basics/03_data-conversions.js
+++ 01_basics/03_data-conversions.js
@@ -1,5 +1,5 @@
-score="33"
-let intscore=Number(score)
-console.log(score)
-
+score="33abcn"
+let intscore=Number(score)// Nan
+console.log(typeof intscore)//output=>> number
 //33=> Number, "33"=> string
+console.log(intscore)//output=>> Nan
