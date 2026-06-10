6ade0bdacaac0f662fc852ef74770dae099e688f
Pravesh <praveshbansh@gmail.com>
2026-06-10 06:02:55 +0000

    02

--- 01_basics/02_datatypes and EMCA.js
+++ 01_basics/02_datatypes and EMCA.js
@@ -1,9 +1,10 @@
 "use strict"; //uses newer version of js
 //alert(3+3)// not working because we're using node not browser
 //there are different datatypes
-// number : 2 to 2^53
-// bigint // null (type= undefined),it is standalone value
-// undefined(type= undefined) //symbol => unique
-//object
+//1.0 (numeric type) number : 2 to 2^53, bigint...
+//2.0 non numeric>> string,boolean,null,undefined,symbol(unique)
+//  undefined(type= undefined),null=null type //
+// null (type= undefined),it is standalone value
+//3.0 object>> object,set,map
 console.log(typeof null)
 console.log(typeof undefined )
