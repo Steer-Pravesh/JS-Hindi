4e89023dcb2a6259e97dd4a3d3289b64764e468f
Pravesh <praveshbansh@gmail.com>
2026-06-10 08:15:13 +0000

    summary

---
+++ 01_basics/data types summary.js
@@ -0,0 +1,21 @@
+//primitive datatypes
+
+//7 types : string,number,bigint,boolean,null,undefined,symbol symbol gives unique id
+
+let id = Symbol(123)
+let anId = Symbol(123)
+//console.log(Number(id) )
+
+//non primitive or reference data types : arrays,objects,functions
+
+group =["Pravesh","p"]
+console.log(typeof myFun)// array typeof = object
+//remeber type of each datatype like null=object; undefiend = undefined 
+obj ={name:"Pravesh",
+    roll:"98"
+}
+ myFun = function(){
+    console.log("hello world")
+ }
+
+ myFun()//type of function = function
