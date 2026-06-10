fac994cb68166f77f7b2f74132ab107da622e214
Pravesh <praveshbansh@gmail.com>
2026-06-10 13:12:46 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+let name =  "praveash"
+let roll = "98"
+console.log("my name is",name) // this is more easy way 
+console.log(`y name is  ${name}`) // we can use `` not '' for ts purpose
+console.log(name.indexOf('a'))//only first occuerence shown....2
+newname=name.substring(0,3)//[0 --> n-1]
+console.log(newname)
